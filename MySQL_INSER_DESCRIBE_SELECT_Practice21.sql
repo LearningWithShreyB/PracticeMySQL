18bd86e63f080fd4f29dfd783d0f0d5dbd95b063
ShreyB <shreypractice.github@gmail.com>
2026-05-01 18:54:14 +0530

    Updates_0105_18:55

---
+++ MySQL_INSER_DESCRIBE_SELECT_Practice21.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (name, email, gender, date_of_birth, salary) VALUES
+('Apoorva', 'apoorva@example.com', 'Female', null, 63000.00),
+('Revansh', 'revansh@example.com', null, '2005-07-14', 35000.00),
+('Prisha', 'prisha@example.com', 'Female', '1999-09-03', null);
+
+DESCRIBE users;
+
+select * from users;
