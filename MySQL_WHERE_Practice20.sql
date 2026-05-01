6672e6a05fbba9f9f6d8557744fe1cf4a3a243b1
ShreyB <shreypractice.github@gmail.com>
2026-05-01 18:46:13 +0530

    Updates_0105_18:47

---
+++ MySQL_WHERE_Practice20.sql
@@ -0,0 +1,13 @@
+-- SELECT * FROM users WHERE gender='MALE';
+
+-- SELECT * FROM users WHERE gender='FEMALE';
+
+-- SELECT * FROM users WHERE SALARY>55000;
+
+-- SELECT * FROM users WHERE SALARY<60000;
+
+-- SELECT * FROM users WHERE salary = 61000;
+
+-- SELECT * FROM users WHERE salary != 61000;
+
+SELECT * FROM users WHERE salary <> 61000;
