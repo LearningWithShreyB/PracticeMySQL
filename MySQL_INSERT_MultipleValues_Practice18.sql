4bf1ac627310465b7510132189e20b49d2ff2e74
ShreyB <shreypractice.github@gmail.com>
2026-05-01 14:38:33 +0530

    Updates_0105_14:39

---
+++ MySQL_INSERT_MultipleValues_Practice18.sql
@@ -0,0 +1,6 @@
+DESCRIBE users;
+INSERT INTO users(Name, Email, DOB, Gender, Cities) VALUES
+('Harsh','Harsh03@gmail.com','2000-06-03','Male', 'Gurugram'),
+('Anamika','Anamika01@gmail.com','1998-01-15','Female', 'Lucknow'),
+('Vaibhav','Vaibhav01@gmail.com','2012-11-28','Male','Ahemdabad');
+SELECT * FROM USERS;
